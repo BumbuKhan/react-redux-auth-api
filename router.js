4260a65fec2dafd042c9dc9b2d3e312c9378e75e
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-25 20:47:59 +0400

    passportjs lib has been wired up with jwt strategy

--- router.js
+++ router.js
@@ -1,5 +1,13 @@
 const Authentication = require('./comntrollers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = function (app) {
+    app.get('/', requireAuth, function (req, res, next) {
+        res.send('Hi there!');
+    });
+
     app.post('/signup', Authentication.signup);
 };
