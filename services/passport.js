1cf2705ee1892eaabc4aa3299af830ab022181a3
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-25 20:57:50 +0400

    added passport localstrategy package

--- services/passport.js
+++ services/passport.js
@@ -3,12 +3,20 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
+const localOptions = {usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
+
+});
+
+// JWT strategy config
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
     secretOrKey: config.secret
 };
 
+// JWT strategy setup
 const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
     User.findById(payload.sub, function (err, user) {
         if (err) {
